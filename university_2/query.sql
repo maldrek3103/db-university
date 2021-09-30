4e5d55f35b3bbbcec16e8f7a2085db1019aabbc6
maldrek3103 <cpisanu96@gmail.com>
2021-09-30 09:30:54 +0200

    made second ex-join

--- university_2/query.sql
+++ university_2/query.sql
@@ -4,3 +4,10 @@ FROM `students`
 RIGHT JOIN `degrees` 
 ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT * 
+FROM `degrees` 
+RIGHT JOIN `departments` 
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
