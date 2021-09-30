640b9cc2481dff15f2ac04eb7a0c8da869d2a8d1
maldrek3103 <cpisanu96@gmail.com>
2021-09-30 16:12:02 +0200

    made bonus ex-join

--- university_2/query.sql
+++ university_2/query.sql
@@ -44,3 +44,16 @@ ON `degrees`.`id` = `courses`.`degree_id`
 JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Matematica'
+
+--BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT `students`.`name`,`students`.`surname`,`courses`.`name`, COUNT(`exam_student`.`vote`) AS `tries`,
+MAX(`exam_student`.`vote`) AS `max_vote`
+FROM `students`
+JOIN `exam_student`
+ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams`
+ON `exams`.`id` = `exam_student`.`exam_id`
+JOIN `courses`
+ON `courses`.`id` = `exams`.`course_id`
+GROUP BY `students`.`id`, `courses`.`id`
+HAVING `max_vote` >= 18
